ee4f4871ed60a7202875a6ddf448a37145e6e898
mertarikanm-gif <mertarikanm@gmail.com>
2026-09-19 10:56:09 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 */
 "use strict";
 
-const APP_VERSION = "2026.09.19a";
+const APP_VERSION = "2026.09.19b";
 const FB_VER = "10.12.2";
 const FB = (m) => `https://www.gstatic.com/firebasejs/${FB_VER}/firebase-${m}.js`;
 
