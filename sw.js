3548a81dc693f40446932568c25b7b602825f512
mertarikanm-gif <mertarikanm@gmail.com>
2026-09-19 15:41:41 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Uygulama kabuğunu önbelleğe alır: internet yokken de açılır.
    Veri senkronu Firestore'un kendi çevrimdışı önbelleğiyle çalışır. */
 
-const CACHE = 'izo-takip-v24';
+const CACHE = 'izo-takip-v25';
 const SHELL = [
   './',
   './index.html',
